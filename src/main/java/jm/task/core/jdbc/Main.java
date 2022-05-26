59e10892f8f32c1488cbae8bd3cd15bd3ffc6ea0
Vladislav Sinelnikov <jobnv86@gmail.com>
2022-05-26 20:06:09 +0500

    Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
 
@@ -10,9 +11,9 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-//        Util.getConnection();
+//        Util.getSessionFactory();
 //
-//        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+//        UserDaoHibernateImpl userDao = new UserDaoHibernateImpl();
 //
 //        userDao.createUsersTable();
 //
